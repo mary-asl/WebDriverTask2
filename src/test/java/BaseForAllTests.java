824576ab2cfe5e41db53bcdb565ae507f5d8196b
Mariya Kazakova <Mariya_Kazakova@epam.com>
2020-08-04 19:10:20 +0600

    correct new task

--- src/test/java/BaseForAllTests.java
+++ src/test/java/BaseForAllTests.java
@@ -1,8 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,8 +19,11 @@ public abstract class BaseForAllTests {
 
     @BeforeClass
     public void initWebDriver() {
-        System.setProperty("webdriver.gecko.driver", "./src/main/resources/gecko/geckodriver.exe");
-        driver = new FirefoxDriver();
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.firefox());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         try{
             driver.get(BASE_URL);
